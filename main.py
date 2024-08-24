7dc0e797165b38793d38238b5c872614aaa72a42
Grant Ludwig <grantludwig97@gmail.com>
2024-08-24 13:12:14 -0700

    loading the sessions properly now

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Firefox()
 try:
     driver.get("https://raceday.watch/#sessions")
     assert "RaceDay.watch" in driver.title
-    elem = driver.find_element(By.CLASS_NAME, "qx-session-item")
+    WebDriverWait(driver, 100).until(
+        lambda driver: driver.find_element(By.CLASS_NAME, "qx-session-item"))
+    elem = driver.find_elements(By.CLASS_NAME, "qx-session-item")
     print("Print", elem)
     elem.clear()
 except Exception as e:
